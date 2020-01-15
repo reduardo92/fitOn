01ca65adae6980c64c7d019c99288cf3ed8a47da
eduardo rivas <r.eduardo92@gmail.com>
2020-01-14 18:15:40 -0600

    updates

--- pages/about.js
+++ pages/about.js
@@ -1,28 +1,8 @@
 import Layout from '../components/layout';
-import ImgBanner from '../components/ui/imgBanner';
 import MapBox from '../components/ui/mapBox';
-import Carousell from '../components/ui/carousel/carousel';
-import GridTwo from '../components/ui/gridTwo';
-import Qoute from '../components/ui/qoute';
 
 const about = () => (
-  <Layout
-    banner='about'
-    heroImg='/outisde.jpg'
-    titleImg='/aboutTitle.svg'
-    heroOpacity='rgba(0, 0, 0, 0.85)'
-  >
-    <ImgBanner
-      img='/patio.jpg'
-      title='MENU'
-      subtile='SEE WHAT WE OFFER'
-      btnTitle='view more'
-      toLink='/menu'
-    />
-    <GridTwo className='mr-a max-width'>
-      <Qoute />
-    </GridTwo>
-    <Carousell />
+  <Layout>
     <MapBox />
   </Layout>
 );
--- pages/contact.js
+++ pages/contact.js
@@ -1,33 +1,9 @@
 import Layout from '../components/layout';
-import ImgBanner from '../components/ui/imgBanner';
 import MapBox from '../components/ui/mapBox';
-import Carousell from '../components/ui/carousel/carousel';
-import GridTwo from '../components/ui/gridTwo';
-import HoursBox from '../components/ui/hoursBox';
-import Qoute from '../components/ui/qoute';
-import Contact from '../components/contact';
 
 const contact = () => (
-  <Layout
-    banner='contact'
-    heroImg='/outisde.jpg '
-    titleImg='/contactTitle.svg'
-    heroOpacity='rgba(0, 0, 0, 0.85)'
-  >
+  <Layout>
     <MapBox />
-    <Contact />
-    <ImgBanner
-      img='/bar_inside_back.jpg'
-      posiion='35%'
-      title='MENU'
-      subtile='SEE WHAT WE OFFER'
-      btnTitle='view more'
-      toLink='/menu'
-    />
-    <GridTwo className='mr-a max-width'>
-      <Qoute />
-    </GridTwo>
-    <Carousell />
   </Layout>
 );
 
